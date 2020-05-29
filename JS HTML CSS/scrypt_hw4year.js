0236db21c2c8d1df32b8552c65deafeadcc3e706
if164ever1 <acere5ihor@gmail.com>
2020-05-29 16:11:35 +0300

    year

--- JS HTML CSS/scrypt_hw4year.js
+++ JS HTML CSS/scrypt_hw4year.js
@@ -1,5 +1,5 @@
 var year = prompt ("Введіть рік: ");
 function isLeap(numbers) {
-    return (year%4==0)? "Це високосний рік " : " Це не високоснийц рік";
+    return (year%4==0 ||  (year%400==0 && !(year%100==0)))? "Це високосний рік " : " Це не високоснийц рік";
 }
 isLeap(year);
